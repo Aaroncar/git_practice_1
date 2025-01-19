7aca6036927c90a92f55335decd8f880b5dab7d1
aaroncar <akampaaaron82@gmail.com>
2025-01-19 10:42:15 +0300

    FEATURE:i have added a factorial.js file

--- src/power/factorial/factorial.js
+++ src/power/factorial/factorial.js
@@ -4,3 +4,67 @@ const factorial = (number) => {
     }
     return number * factorial(number - 1);
   };
+  const firstTwentyNumber = (n) => {
+    let i = 0;
+    while (i < n) {
+      if (i % 2 === 0) {
+        return i;
+      }
+    }
+  };
+  ww
+  const firstTwentyNumber = (n) => {
+    let i = 0;
+    while (i < n) {
+      if (i % 2 === 0) {
+        return i;
+      }
+    }
+  };
+  ww
+  const firstTwentyNumber = (n) => {
+    let i = 0;
+    while (i < n) {
+      if (i % 2 === 0) {
+        return i;
+      }
+    }
+  };
+  ww
+  const firstTwentyNumber = (n) => {
+    let i = 0;
+    while (i < n) {
+      if (i % 2 === 0) {
+        return i;
+      }
+    }
+  };
+  ww
+  const firstTwentyNumber = (n) => {
+    let i = 0;
+    while (i < n) {
+      if (i % 2 === 0) {
+        return i;
+      }
+    }
+  };
+  const firstTwentyNumber = (n) => {
+    let i = 0;
+    while (i < n) {
+      if (i % 2 === 0) {
+        return i;
+      }
+    }
+  };
+  const firstTwentyNumber = (n) => {
+    let i = 0;
+    while (i < n) {
+      if (i % 2 === 0) {
+        return i;
+      }
+    }
+  };
+  
+    
+  
+   
